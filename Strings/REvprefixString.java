98decbea8d910792dea5af49ff86ea9dc45067bf
mohammedarbaz119 <mohammedarbazq16@gmail.com>
2022-11-03 23:06:11 +0530

    added String problem

---
+++ Strings/REvprefixString.java
@@ -0,0 +1,29 @@
+public class REvprefixString {
+    public static void main(String[] args) {
+        String a = "abcdefg";
+        char b ='d';
+        int k=-1;
+        String c="";
+
+
+        for (int i = 0; i < a.length(); i++) {
+            if(a.charAt(i)==b){
+k=i;
+break;
+
+            }
+        }
+            if(k==-1){
+                System.out.println(a);
+                return;
+            }
+            else{
+for (int j = k; j >=0; j--) {
+    c+=a.charAt(j);
+}
+c+=a.substring(k+1);
+System.out.println(c);
+            }
+        }
+    }
+
