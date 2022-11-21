794f456cc7e1b0c620673e85fd1351951c9a84ea
mohammedarbaz119 <mohammedarbazq16@gmail.com>
2022-11-21 23:21:33 +0530

    .

--- Strings/Substring3.java
+++ Strings/Substring3.java
@@ -1,5 +1,11 @@
 public class Substring3 {
+    static int pcu(int a) {
+        return a;
+    }
+
     public static void main(String[] args) {
+        Substring3 s = new Substring3();
+        System.out.println(s.pcu(0));
         String a = "abdddeefgfe";
         int c = 0;
         for (int i = 0; i < a.length() - 2; i++) {
